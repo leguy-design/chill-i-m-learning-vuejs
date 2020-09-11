7adbb169a5286d15d7018aceda2d0cfa9b586059
leguy-design <leguy.val@gmail.com>
2020-09-11 22:39:16 +0200

    first commit

---
+++ resources/js/components/ChildComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <span>ici: {{ foo }}</span>
+    </div>
+</template>
+<script>
+    export default {
+        data: function () {
+            return {
+                amount: 0,
+                dummyData: [],
+            }
+        },
+        props: [
+            'foo'
+        ],
+    }
+</script>
